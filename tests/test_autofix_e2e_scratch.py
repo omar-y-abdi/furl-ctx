87352d0fc2bc06d000f701317ad4a2949d039163
furl-autofix <furl-autofix@users.noreply.github.com>
2026-07-18 22:08:22 +0000

    ci: autofix formatting and lint

--- tests/test_autofix_e2e_scratch.py
+++ tests/test_autofix_e2e_scratch.py
@@ -1,8 +1,6 @@
 """Scratch file for the autofix end-to-end drill. This PR is closed, never merged."""
 
-import os
 
-
-def test_autofix_scratch( ):
-    x=[1,2,3]
-    assert sum( x )==6
+def test_autofix_scratch():
+    x = [1, 2, 3]
+    assert sum(x) == 6
